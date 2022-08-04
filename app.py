637a342b85aae1b260805ad6a486b1d88e2f075b
Hartomeister <64258322+Hartomeister@users.noreply.github.com>
2022-08-04 03:22:18 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask,render_template,request,redirect
+import smtplib
+
+app=Flask(__name__)
+
+#route() decorators
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/index.html')
+def index():
+    return render_template('index.html')
+
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
+
+
+if __name__=='__main__':
+    app.run(debug=True)
