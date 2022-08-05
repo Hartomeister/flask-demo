052c6e4d8091c90dfc3463457c901cc29d7df38c
Hartomeister <64258322+Hartomeister@users.noreply.github.com>
2022-08-05 11:01:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,14 +7,13 @@ app=Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
-
 @app.route('/index.html')
 def index():
     return render_template('index.html')
 
-@app.route('/id.html')
+
 def about():
-    return render_template('id.html')
+ 
 
 
 
